f8c09241041041b0283dd9620ccfe652b3898a6a
Guilherme Santana <guilherme.santana@automatizai.com.br>
2020-09-22 19:24:18 -0300

    Implementação de testes que rodam na nuvem.

--- src/main/java/curso_appium/core/DriverFactory.java
+++ src/main/java/curso_appium/core/DriverFactory.java
@@ -3,6 +3,7 @@ package curso_appium.core;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,28 +15,43 @@ import io.appium.java_client.remote.MobileCapabilityType;
 public class DriverFactory {
 
 	public static AndroidDriver<MobileElement> driver;
-	
-	private static AndroidDriver<MobileElement> createDriver() {
 
+	private static void createDriver() {
+		File diretorioAplicacao = new File("apps");
+		File arquivoAplicacao = new File(diretorioAplicacao, "app-release.apk");
+		DesiredCapabilities capacidade = new DesiredCapabilities();
+		capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+		capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+		capacidade.setCapability("appPackage", "br.com.liveloincentivo");
+		capacidade.setCapability(MobileCapabilityType.APP, arquivoAplicacao.getAbsolutePath());
 		try {
-			File diretorioAplicacao = new File("apps");
-			File arquivoAplicacao = new File(diretorioAplicacao, "app-release.apk");
-			DesiredCapabilities capacidade = new DesiredCapabilities();
-			capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-			capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
-			capacidade.setCapability("appPackage", "br.com.liveloincentivo");
-			capacidade.setCapability(MobileCapabilityType.APP, arquivoAplicacao.getAbsolutePath());
 			driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capacidade);
 
 		}catch (MalformedURLException e){
 			System.out.println( e.getMessage() );
 		}
-		return driver;
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
+	private static void createTestObjectDriver() {
+		DesiredCapabilities capacidade = new DesiredCapabilities();
+		capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+		capacidade.setCapability("testobject_apikey", "91E7CR91E7CR91E7CR91E7CR91E7CR");
+		capacidade.setCapability("automationName", "uiautomator2");
+		capacidade.setCapability("appiumVersion", "1.7.2");
+		try {
+			driver = new AndroidDriver<MobileElement>(new URL("https://us1.appium.testobject.com/wd/hub"), capacidade);
+
+		}catch (MalformedURLException e){
+			System.out.println( e.getMessage() );
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
 	public static AndroidDriver<MobileElement> getDriver() {
 		if(driver == null) {
-			createDriver();
+			//createDriver();
+			createTestObjectDriver();
 		}
 		return driver;
 	}
--- src/main/java/curso_appium/page/MenuPage.java
+++ src/main/java/curso_appium/page/MenuPage.java
@@ -1,11 +1,75 @@
 package curso_appium.page;
+import static curso_appium.core.DriverFactory.getDriver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Dimension;
+
 import curso_appium.core.DSL;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 public class MenuPage {
 	
 	private DSL dsl = new DSL();
 	
 	public void acessarFormulario() {
 		dsl.clicarPorTexto("Formulário");
+	}
+	
+	
+	public void clicarSeekBar(double position) {
+		MobileElement seek = getDriver().findElement(MobileBy.AccessibilityId("slid"));
+		int y = seek.getLocation().y + (seek.getSize().height / 2);
+		int x = (int) (seek.getLocation().x + (seek.getSize().width * position));
+		
+		tap(x, y);
+	}
+	
+	public void tap(int x, int y) {
+		new TouchAction(getDriver()).tap(x, y);	
+	}
+	
+	public void scroll(double inicio, double fim) {
+		Dimension size = getDriver().manage().window().getSize();
+		
+		int x = size.width /2;
+		int start_y = (int) (size.height * inicio);
+		int end_y = (int) (size.height * fim);
+		
+		new TouchAction(getDriver())
+		.press(x, start_y)
+		.waitAction(Duration.ofMillis(500))
+		.moveTo(x, end_y)
+		.release()
+		.perform();
+	}
+	
+	public void swipe(double inicio, double fim) {
+		Dimension size = getDriver().manage().window().getSize();
+		
+		int y = size.height /2;
+		int start_x = (int) (size.width * inicio);
+		int end_x = (int) (size.width * fim);
+		
+		new TouchAction(getDriver())
+		.press(start_x, y)
+		.waitAction(Duration.ofMillis(500))
+		.moveTo(end_x, y)
+		.release()
+		.perform();
+	}
+	
+	public void swipeElement(MobileElement element, double inicio, double fim) {
+		int y = element.getLocation().y + (element.getSize().height /2);
+		int start_x = (int) (element.getSize().width * inicio);
+		int end_x = (int) (element.getSize().width * fim);
+		new TouchAction(getDriver())
+		.press(start_x, y)
+		.waitAction(Duration.ofMillis(500))
+		.moveTo(end_x, y)
+		.release()
+		.perform();
 	}
 
 }
